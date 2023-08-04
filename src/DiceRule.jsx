0fee5e50ee8f394fa210cff34d0ea0b43100b4d5
unknown <badrudavid201@gmail.com>
2023-08-04 06:04:39 -0700

    Made some updates and made the app reponsive

--- src/DiceRule.jsx
+++ src/DiceRule.jsx
@@ -4,7 +4,7 @@ export default function DiceRule(){
         <div className="DiceRule">
             <h2>Dice Game</h2>
             <h4>Game Rule: Once a player rolls a Twin (e.g a 2-2, a 6-6), the player automatically wins.</h4>
-            <p>Each player is to refresh the page or click the <b><i>Roll Die</i></b> button to play.</p>
+            <p>Each player is to click the <b><i>Roll Die</i></b> button to play.</p>
         </div>
     )
 }
--- src/DieRoll.jsx
+++ src/DieRoll.jsx
@@ -3,31 +3,52 @@ import "./DieRoll.css"
 export default function DieRoll(){
 const roll1 = Math.floor(Math.random() * 6) + 1;
 const roll2 = Math.floor(Math.random() * 6) + 1;
+const reRoll1 = Math.floor(Math.random() * 6) + 1;
+const reRoll2 = Math.floor(Math.random() * 6) + 1;
+
 const isWinner = roll1 === roll2;
+const isWinner2 = reRoll1 === reRoll2;
+
 const [roll, setRoll] = useState(roll1)
 const [reroll, setReroll] = useState(roll2)
-const getRoll = () => {
+
+const [rollover, setRollover] = useState(reRoll1)
+const [rerollover, setRerollover] = useState(reRoll2)
+
+const getRoll1 = () => {
+    const roll1 = Math.floor(Math.random() * 6) + 1;
+    const roll2 = Math.floor(Math.random() * 6) + 1;
     setRoll(roll1);
     setReroll(roll2);
+};
+const getRoll2 = () => {
+    const reRoll1 = Math.floor(Math.random() * 6) + 1;
+    const reRoll2 = Math.floor(Math.random() * 6) + 1;
+    setRollover(reRoll1);
+    setRerollover(reRoll2);
 };
 
     return(
         <div className="DieRoll">
              <div className="p1">
                  <h1 style={{color: isWinner? "white" : "red"}}>PLAYER 1</h1>
-                 <h2 style={{color: isWinner? "white" : "red"}}>DIE 1: {roll}</h2>
-                 <h2 style={{color: isWinner? "white" : "red"}}>DIE 2: {reroll}</h2>
-                <button onClick={getRoll}>ROLL DIE</button>
+                 <div className="score">
+                    <h2 style={{color: isWinner? "white" : "red"}}>DIE 1: {roll1}</h2>
+                    <h2 style={{color: isWinner? "white" : "red"}}>DIE 2: {roll2}</h2>
+                </div>
+                <button onClick={getRoll1}>ROLL DIE</button>
                 {isWinner && <h3 style={{color:"white"}}>YOU WON!!! :)</h3>}
                 {isWinner && <h4 style={{color:"white"}}>CONGRATULATIONS</h4>}
             </div>
             <div className="p2">
-                 <h1 style={{color: isWinner? "white" : "red"}}>PLAYER 2</h1>
-                 <h2 style={{color: isWinner? "white" : "red"}}>DIE 1: {roll}</h2>
-                 <h2 style={{color: isWinner? "white" : "red"}}>DIE 2: {reroll}</h2>
-                <button onClick={getRoll}>ROLL DIE</button>
-                {isWinner && <h3 style={{color:"white"}}>YOU WON!!! :)</h3>}
-                {isWinner && <h4 style={{color:"white"}}>CONGRATULATIONS</h4>}
+                 <h1 style={{color: isWinner2? "white" : "red"}}>PLAYER 2</h1>
+                 <div className="score">
+                    <h2 style={{color: isWinner2? "white" : "red"}}>DIE 1: {reRoll1}</h2>
+                    <h2 style={{color: isWinner2? "white" : "red"}}>DIE 2: {reRoll2}</h2>
+                 </div>
+                 <button onClick={getRoll2}>ROLL DIE</button>
+                {isWinner2 && <h3 style={{color:"white"}}>YOU WON!!! :)</h3>}
+                {isWinner2 && <h4 style={{color:"white"}}>CONGRATULATIONS</h4>}
             </div>
         </div>
     )
